6ee744de7cea76d7f0a382dfa5bcc55c5753b274
Hieutrinh04 <167523540+Hieutrinh04@users.noreply.github.com>
2024-05-06 09:38:02 +0700

    Add files via upload

---
+++ DTO/fees.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuongTrinhQuanLyKyTucXa_Version2.Entities
+{
+    internal class fees
+    {
+    }
+}
